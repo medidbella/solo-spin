7b57089e91315de89f863e55f9d6c687b0db4d27
AliAkouhar <akouhar04ali@gmail.com>
2025-12-12 14:39:21 +0100

    adding signUp page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts}"
-  ],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        solo: ["SoloLeveling", "serif"],
+        teachers: ["Teachers", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
