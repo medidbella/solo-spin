452b48db4aa6b7fb444b10d8a57ad7b9f623e1d0
AliAkouhar <akouhar04ali@gmail.com>
2026-01-01 14:33:01 +0100

    adding profile paga , and fixing navigation between pages

--- frontend/src/pages/leaderBoard.ts
+++ frontend/src/pages/leaderBoard.ts
@@ -1,51 +1,39 @@
 import { renderHeader } from "../components/Header";
 import { renderSideBar } from "../components/SideBar";
 
-// =========================================================================
-// 1. TYPE DEFINITION (L'Plan)
-// =========================================================================
-// Hna kanwriw l TypeScript kifach "Player" dayr.
-// Ila nssina chi 7aja (mitalan nssina score), TS ghadi y7bsna.
 interface Player {
-  rank: number;   // Tartib (1, 2, 3...)
-  name: string;   // Smiya (username)
-  score: number;  // Ni9at (XP)
-  avatar: string; // Lien dyal tswira
+  rank: number;
+  name: string;
+  score: number;
+  avatar: string;
 }
 
-// =========================================================================
-// 2. MAIN FUNCTION
-// =========================================================================
+
 export function renderLeaderBoard(): string {
   
-  // -- MOCK DATA (Données d'exemple) --
-  // Hado homa li ghadi yjiw mn Backend mn b3d (JSON).
-  // Daba kandirohom "Hardcoded" bach nzw9o l page.
-  const allPlayers: Player[] = [
-    { rank: 1, name: "Aakouhar", score: 6450, avatar: "/img/avatar1.png" },
-    { rank: 2, name: "Two", score: 6009, avatar: "/img/avatar2.png" },
-    { rank: 3, name: "Three", score: 5990, avatar: "/img/avatar3.png" },
-    { rank: 4, name: "username1", score: 5675, avatar: "/img/avatar4.png" },
-    { rank: 5, name: "username2", score: 5497, avatar: "/img/avatar5.png" },
-    { rank: 6, name: "username3", score: 5097, avatar: "/img/avatar6.png" },
-    { rank: 6, name: "username4", score: 5090, avatar: "/img/avatar7.png" },
-    { rank: 6, name: "username5", score: 5070, avatar: "/img/avatar9.png" },
-    { rank: 6, name: "username6", score: 5060, avatar: "/img/avatar8.png" },
+const allPlayers: Player[] = [
+    { rank: 1, name: "Aakouhar", score: 6450, avatar: "../../public/imgs/nigga.png" },
+    { rank: 2, name: "Two", score: 6009, avatar: "../../public/imgs/sung.png" },
+    { rank: 3, name: "Three", score: 5990, avatar: "../../public/imgs/orange-boy.png" },
+    { rank: 4, name: "username1", score: 5675, avatar: "../../public/imgs/nigga.png" },
+    { rank: 5, name: "username2", score: 5497, avatar: "../../public/imgs/nigga.png" },
+    { rank: 6, name: "username3", score: 5097, avatar: "../../public/imgs/nigga.png" },
+    { rank: 7, name: "username4", score: 5090, avatar: "../../public/imgs/nigga.png" },
+    { rank: 8, name: "username5", score: 5070, avatar: "../../public/imgs/nigga.png" },
+    { rank: 9, name: "username6", score: 5060, avatar: "../../public/imgs/nigga.png" },
   ];
 
-  // -- LOGIC (T9sam dyal data) --
-  // .slice(0, 3) -> Haz 3 lwala (0, 1, 2) bach n7ttohom f podium.
-  const topThree = allPlayers.slice(0, 3);
+ const topThree = allPlayers.slice(0, 3);
   
-  // .slice(3) -> Haz mn 3 tal lkher bach n7ttohom f list ta7t.
+
   const restList = allPlayers.slice(3);
 
-  // Kankhdo 3 lwala b variable sahl bach nktbohom f HTML.
-  const first = topThree[0];  // Mol Rank 1
-  const second = topThree[1]; // Mol Rank 2
-  const third = topThree[2];  // Mol Rank 3
 
-  // -- HTML RETURN --
+  const first = topThree[0];
+  const second = topThree[1];
+  const third = topThree[2];
+
+  
   return /* html */`
     <main class="h-screen flex flex-col bg-[#0e071e] text-white font-sans overflow-hidden">
       
@@ -58,12 +46,12 @@ export function renderLeaderBoard(): string {
         <section class="flex-1 overflow-y-auto p-8 relative">
             
             <h1 class="text-center text-3xl font-bold mb-10 tracking-widest uppercase">Leaderboard</h1>
-
-            <div class="flex justify-center items-end gap-6 mb-16">
-                
+            <!-- the first three wiiners div -->
+            <div class="flex justify-center items-end gap-7 mb-16">
+                <!-- second winner -->
                 <div class="flex flex-col items-center">
                     <div class="relative">
-                        <img src="${second.avatar}" class="w-20 h-20 rounded-full border-2 border-indigo-500 object-cover shadow-[0_0_15px_rgba(99,102,241,0.5)]">
+                        <img src="${second.avatar}" class="w-20 h-20 rounded-full border-2 border-indigo-500 object-contain shadow-[0_0_20px_rgba(99,102,241,0.5)]">
                         
                         <div class="absolute -bottom-3 left-1/2 -translate-x-1/2 bg-[#0e071e] border border-indigo-500 px-2 rounded-full text-xs">
                             2
@@ -72,13 +60,13 @@ export function renderLeaderBoard(): string {
                     <h3 class="mt-4 font-bold text-sm text-indigo-100">${second.name}</h3>
                     <p class="text-indigo-400 text-xs">${second.score}</p>
                 </div>
-
+                <!-- first winner the king -->
                 <div class="flex flex-col items-center -mt-8"> 
                     
                     <div class="text-4xl mb-2 animate-bounce">👑</div> 
                     
                     <div class="relative">
-                        <img src="${first.avatar}" class="w-28 h-28 rounded-full border-4 border-yellow-400 object-cover shadow-[0_0_20px_rgba(250,204,21,0.6)]">
+                        <img src="${first.avatar}" class="w-28 h-28 rounded-full border-4 border-yellow-400 object-cover shadow-[0_0_30px_rgba(250,204,21,0.6)]">
                          
                          <div class="absolute -bottom-3 left-1/2 -translate-x-1/2 bg-[#0e071e] border border-yellow-400 text-yellow-400 px-3 rounded-full font-bold">
                             1
@@ -87,10 +75,10 @@ export function renderLeaderBoard(): string {
                     <h3 class="mt-4 font-bold text-lg text-yellow-100">${first.name}</h3>
                     <p class="text-yellow-400 font-bold">${first.score}</p>
                 </div>
-
+                <!-- third winner-->
                 <div class="flex flex-col items-center">
                     <div class="relative">
-                        <img src="${third.avatar}" class="w-20 h-20 rounded-full border-2 border-orange-500 object-cover shadow-[0_0_15px_rgba(249,115,22,0.5)]">
+                        <img src="${third.avatar}" class="w-20 h-20 rounded-full border-2 border-orange-500 object-cover shadow-[0_0_20px_rgba(249,115,22,0.5)]">
                         
                         <div class="absolute -bottom-3 left-1/2 -translate-x-1/2 bg-[#0e071e] border border-orange-500 px-2 rounded-full text-xs">
                             3
@@ -101,7 +89,7 @@ export function renderLeaderBoard(): string {
                 </div>
 
             </div>
-
+            <!-- other winners -->
             <div class="flex flex-col gap-4 max-w-2xl mx-auto w-full pb-10">
                 
                 ${restList.map((player) => {
@@ -111,7 +99,7 @@ export function renderLeaderBoard(): string {
                         <div class="absolute left-0 z-10 w-12 h-12 bg-indigo-900 border border-indigo-500 rotate-45 flex items-center justify-center shadow-[-5px_+5px_0px_#441563] group-hover:bg-indigo-600 transition-colors">
                             
                             <span class="-rotate-45 font-bold text-white text-sm">
-                                0${player.rank}
+                                ${player.rank}
                             </span>
                         </div>
 
