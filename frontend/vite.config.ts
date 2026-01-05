885873d39d6f72e322b1272f2231f40e9a49e77c
Badered10 <badr8aouragh@gmail.com>
2026-01-05 21:02:45 +0100

    Rebuild a Major version of Makefile that are have giude of use an better interface and explaination of each rule , and fixes the deadlock issue on docker compose , also fix configuratuion of vite so that devleopers may run make dev and see there changes live in ngnix !

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,15 +4,15 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [tailwindcss()],
   server: {
+    watch: {
+      usePolling: true,
+    },
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
     hmr: {
       protocol: 'wss',
       clientPort: 8443, 
-    },
-    watch: {
-      usePolling: true,
     },
   },
 });
