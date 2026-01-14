c58988c0fb0c872a5e137d72a8e440f6645380ba
AliAkouhar <akouhar04ali@gmail.com>
2026-01-14 23:29:05 +0100

    adding logout logic

--- frontend/src/components/Header.ts
+++ frontend/src/components/Header.ts
@@ -6,10 +6,32 @@ export function renderHeader() : string{
             <button aria-label="notifications">
               <i class="fa-regular fa-bell text-[#2A3FA1] cursor-pointer hover:text-white hover:scale-105 duration-200"></i>
             </button>
-            <button aria-label="logout">
+            <button id="logoutButton" aria-label="logout">
               <i class="fa-solid fa-arrow-right-from-bracket text-[#2A3FA1] cursor-pointer hover:text-white hover:scale-105 duration-200"></i>
             </button>
           </div>
         </header>
     `;
+}
+
+export function setupHeaderLogic() : void
+{
+  const logoutBtn = document.getElementById("logoutButton");
+  if (!logoutBtn) return;
+
+  logoutBtn.addEventListener('click', async() => {
+    try{
+      const response = await fetch('api/logout', {
+        method: 'POST',
+      });
+      if (response.ok)
+      {
+        window.location.href = '/login'; //redirect my user to login and midbella handles the remove of tokens
+      }
+    }
+    catch (error)
+    {
+
+    }
+  });
 }
