f86884080e623d36ac9b8f8670eeb9f7783c0760
Badered10 <badr8aouragh@gmail.com>
2026-01-18 06:53:59 +0100

    infra: modularize docker-compose and add chat container
    - Split ELK into docker-compose.elk.yml.
    - Updated Makefile logic for dev/prod/without modes.
    - Chat service is now containerized and connected to Nginx.
    - Fixed hot-reload issues in chat dev mode.

--- chat/src/index.ts
+++ chat/src/index.ts
@@ -10,7 +10,7 @@ fastify.get('/working', async () => { //ghir bach ntesti
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 4000, host: '0.0.0.0' });
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
