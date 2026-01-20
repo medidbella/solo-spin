718437e88afb964dd649dc77316600be927d048e
AliAkouhar <akouhar04ali@gmail.com>
2026-01-20 17:58:11 +0100

    done with UI of profiles page and its sections like friend request and block sections and test them with a mock data  the next step is integrate it with backend

--- frontend/src/pages/profiles.ts
+++ frontend/src/pages/profiles.ts
@@ -26,19 +26,19 @@ function getTabContent(): string {
 
 //this function built to return the stylin of class bar based on its state
 function getTabClasses(tab: TabType) : string{
-  const baseClasses = "flex-1 py-4  text-[#f2f2f2] text-center font-semibold text-lg shadow-[-5px_-5px_0px_#441563]";
+  const baseClasses = "flex-1 py-4 font-[#solo] cursor-pointer text-[#f2f2f2] text-center font-semibold text-lg shadow-[-5px_-5px_0px_#441563]";
 
   if (tab === activeTab)
   {
-    return `${baseClasses} bg-[3b82f6]`;
+    return `${baseClasses} bg-[#2A3FA1]`;
   }
-  return `${baseClasses} bg-[6b2f8a]`;
+  return `${baseClasses} bg-[#441563]`;
 }
 
 //this function render only the navbar with a costom style for the active tab to make special between other tabs
 function renderTabBar(): string {
   return /* html */ `
-    <div class="flex rounded-t-lg overflow-hidden">
+    <div class="flex  overflow-hidden">
       <button
         class="${getTabClasses("friends")}"
         data-tab="friends"
@@ -64,11 +64,12 @@ function renderTabBar(): string {
 
 //this is an update function that will change only the centred contaired that hold all lists
 function updateTabUI() : void{
-  const tabBar = document.getElementById("profiles-tab-bat");
+  const tabBar = document.getElementById("profiles-tab-bar");
   const contentArea = document.getElementById("profiles-content");
 
   if (tabBar)
   {
+    console.log("i am here");
     tabBar.innerHTML = renderTabBar();
   }
   if (contentArea)
@@ -77,14 +78,55 @@ function updateTabUI() : void{
   }
 }
 
+export function setupProfilesPageLogic() : void {
+  const container = document.getElementById("profiles-container");
+  if (!container) return;
 
+  container.addEventListener("click", (e) =>{
+    const target = e.target as HTMLLinkElement;
+    //handele tab switching to style active and non active tabs
+    const tabButton = target.closest("[data-tab") as HTMLElement;
+    if (tabButton)
+    {
+      //getting here the object buttong that the user want to navigate to its content
+      const newTab = tabButton.dataset.tab as TabType;
+      if (newTab && newTab !== activeTab)
+      {
+        activeTab = newTab;
+        updateTabUI();
+      }
+      return;
+    }
+    //handele all action buttons
+    const actionButton = target.closest("[data-action]") as HTMLElement;
+    if (actionButton)
+    {
+      const action = actionButton.dataset.action;
 
+      switch(action){
+        case "remove-friend":
+          const friendshipId = actionButton.dataset.friendshipId;
+          console.log(`here i should call the api using this friendshipId  ${friendshipId}`);
+          break;
+        case "accept-request":
+          const requestId = actionButton.dataset.requestId;
+          console.log(`call here an api using post methode here is the friendshipId ${requestId}`);
+          break;
+        case "reject-request":
+          const rejectId = actionButton.dataset.requestId;
+          console.log(`a post methode should treat here later here is the requestId should be rejected ${rejectId}`);
+          break;
+        case "unblock-user":
+          const userId = actionButton.dataset.userId;
+          console.log(`TODO: post request to block with friendId ${userId}`);
+      }
+    }
+  });
+}
 
-
-
-
-
+//the main rendering profiles function
 export function renderProfilesPage(): string {
+  activeTab = "friends" //make the content list as a defualt
   return /* html */`
     <main class="h-screen flex flex-col bg-[#0e071e] text-white font-sans overflow-hidden">
       
@@ -94,8 +136,19 @@ export function renderProfilesPage(): string {
         
         ${renderSideBar()}
         
-        <section class="flex-1 overflow-y-auto p-8 relative">
-            <h1 class="text-5xl text-red-700" >profiles page in progress</h1>
+        <section class="flex-1 overflow-y-auto p-8 items-center">
+        <!-- THIS IS THE MAIN CONTAINER -->
+        <div id="profiles-container" class="max-w-4xl mx-auto bg-[#2A3FA1] shadow-[-10px_-10px_0px_#441563]">
+          <!-- Tab Bar -->
+          <div id="profiles-tab-bar">
+            ${renderTabBar()}
+          </div>
+          <!-- Content Area -->
+          <div id="profiles-content" class="bg-[#2A3FA1] rounded-b-lg p-6 min-h-[400px]">
+            ${getTabContent()}
+          </div>
+        </div>
+
         </section>
       </div>
     </main>
